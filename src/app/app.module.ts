dec6a81a74c5e4f81ec0005d263749d60e71ad6f
Rajpreet Singh Nayyar <rnayyar@tavisca.com>
2018-09-02 15:02:40 +0530

    first comit

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing/app-routing.module';
+import { AppComponent } from './app.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
+import { ButtonDescriptionComponent } from './button-description/button-description.component';
+import { InputComponent } from './input/input.component';
+import { PanelComponent } from './panel/panel.component';
+import { ButtontypeComponent } from './buttontype/buttontype.component';
+import { CalenderComponent } from './calender/calender.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SideBarComponent,
+    ButtonDescriptionComponent,
+    InputComponent,
+    PanelComponent,
+    ButtontypeComponent,
+    CalenderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
